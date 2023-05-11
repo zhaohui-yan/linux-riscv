c6ac97458ad85a1fceb077173cd3412cf43d79ff
xiaoguang.xing <xiaoguang.xing@sophgo.com>
2023-05-11 11:36:18 +0800

    Revert "riscv: Workaroud for cpu still using i-utlb when into m mode"
    Disable l0btb|rs bits in MHCR to workaroud for cpu still using i-utlb
    
    This reverts commit 13ee23bdb3c187689099c1cd49de68932d36b643.

--- arch/riscv/kernel/sbi.c
+++ arch/riscv/kernel/sbi.c
@@ -37,7 +37,6 @@ struct sbiret sbi_ecall(int ext, int fid, unsigned long arg0,
 	register uintptr_t a5 asm ("a5") = (uintptr_t)(arg5);
 	register uintptr_t a6 asm ("a6") = (uintptr_t)(fid);
 	register uintptr_t a7 asm ("a7") = (uintptr_t)(ext);
-	asm volatile ("sfence.vma	zero, t0");
 	asm volatile ("ecall"
 		      : "+r" (a0), "+r" (a1)
 		      : "r" (a2), "r" (a3), "r" (a4), "r" (a5), "r" (a6), "r" (a7)
