69ad2cf9405aaf94ac2d1f48a206f9def733aa29
zhaohui-yan <zhaohui.yan@sophgo.com>
2023-11-08 09:39:53 +0800

    drivers: pcie: Add switchtec to the PCIE MSIX whitelist riscv: dts: sophgo: Set PCIE1 to support msix
    Signed-off-by: zhaohui-yan <zhaohui.yan@sophgo.com>

--- drivers/pci/controller/cadence/pcie-cadence-sophgo.c
+++ drivers/pci/controller/cadence/pcie-cadence-sophgo.c
@@ -463,6 +463,7 @@ static struct msi_domain_info cdns_pcie_top_intr_msi_domain_info = {
 struct vendor_id_list vendor_id_list[] = {
 	{"Inter X520", 0x8086},
 	{"WangXun RP1000", 0x8088},
+	{"Switchtec", 0x11f8},
 };
 
 size_t vendor_id_list_num = ARRAY_SIZE(vendor_id_list);
