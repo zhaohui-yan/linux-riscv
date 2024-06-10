7cb1cba6b2993aa111097bf28a3a35db5d638e5f
Han Gao <gaohan@iscas.ac.cn>
2024-06-10 18:55:40 +0800

    xtheadvector: fix it used as v-ext when hwprobe is used
    Signed-off-by: Han Gao <gaohan@iscas.ac.cn>

--- arch/riscv/include/asm/vector.h
+++ arch/riscv/include/asm/vector.h
@@ -29,6 +29,11 @@ static __always_inline bool has_vector(void)
 	return riscv_has_extension_unlikely(RISCV_ISA_EXT_v);
 }
 
+static __always_inline bool has_xtheadvector(void)
+{
+        return riscv_has_extension_unlikely(RISCV_ISA_EXT_XTHEADVECTOR);
+}
+
 static inline void __riscv_v_vstate_clean(struct pt_regs *regs)
 {
 	unsigned long sr_vs, sr_vs_clean;
@@ -297,6 +302,7 @@ struct pt_regs;
 
 static inline int riscv_v_setup_vsize(void) { return -EOPNOTSUPP; }
 static __always_inline bool has_vector(void) { return false; }
+static __always_inline bool has_xtheadvector(void) { return false; }
 static inline bool riscv_v_first_use_handler(struct pt_regs *regs) { return false; }
 static inline bool riscv_v_vstate_query(struct pt_regs *regs) { return false; }
 static inline bool riscv_v_vstate_ctrl_user_allowed(void) { return false; }
